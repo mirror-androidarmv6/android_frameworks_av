07c3cb9a39bc108c64adec3eee9c3a78f3b5ef4c
sakindia123 <sa6915@gmail.com>
2013-07-02 08:43:50 +0100

    CameraParameters: Remove duplicate if-def's
    Fixes compile error on HTC MSM7x27A
    
    Change-Id: Ib0b6b5079779cebf027ab687d3173180c7aa5c8f

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -949,25 +949,16 @@ public:
     static const char KEY_MIN_SHARPNESS[];
 #endif
     static const char KEY_MAX_SHARPNESS[];
-#ifdef QCOM_LEGACY_CAM_PARAMS
-    static const char KEY_MIN_SHARPNESS[];
-#endif
     static const char KEY_CONTRAST[];
 #ifdef QCOM_HARDWARE
     static const char KEY_MIN_CONTRAST[];
 #endif
     static const char KEY_MAX_CONTRAST[];
-#ifdef QCOM_LEGACY_CAM_PARAMS
-    static const char KEY_MIN_CONTRAST[];
-#endif
     static const char KEY_SATURATION[];
 #ifdef QCOM_HARDWARE
     static const char KEY_MIN_SATURATION[];
 #endif
     static const char KEY_MAX_SATURATION[];
-#ifdef QCOM_LEGACY_CAM_PARAMS
-    static const char KEY_MIN_SATURATION[];
-#endif
 
     static const char KEY_HISTOGRAM[] ;
     static const char KEY_SUPPORTED_HISTOGRAM_MODES[] ;
